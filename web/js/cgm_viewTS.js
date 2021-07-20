303aba6dcbdfa8d2ad2d04cf6c1dbbee18ff331d
Mei-Hui Su <mei@MeiPro.local>
2021-07-19 18:10:01 -0700

    add more cgm_viewTS code

--- web/js/cgm_viewTS.js
+++ web/js/cgm_viewTS.js
@@ -115,7 +115,92 @@ function showTSView(urls,fnames,ftypes) {
   set_PARAMS(params);
 
   window.console.log("ShowTSView, params >>"+params);
-  
   $('#viewTSIfram').attr('src',"cgm_ts.html?"+params);
 
 }
+
+// should be able to track the initial state and then return to it
+function refreshTSview() {
+
+  resetReprTSview();
+  resetExpandTSview();
+
+  var params=get_PARAMS();
+  $('#viewTSIfram').attr('src',"cgm_ts.html?"+params);
+}
+
+
+// move current popup modal to a new tab
+function moveTSview() {
+  var yourDOCTYPE = "<!DOCTYPE html>"; // your doctype declaration
+  var copyPreview = window.open('about:blank', 'CGM PlotTS', "resizable=yes,scrollbars=yes,status=yes");
+  var newCopy = copyPreview.document;
+  newCopy.open();
+  // remove copy and new tab buttons
+  document.getElementById("viewTSClosebtn").style.display="none";
+  document.getElementById("viewTSMovebtn").style.display="none";
+  var newInner=document.documentElement.innerHTML;
+  newCopy.write(yourDOCTYPE+"<html>"+ newInner+ "</html>");
+  newCopy.close();
+  document.getElementById("viewTSClosebtn").style.display="block";
+  document.getElementById("viewTSMovebtn").style.display="block";
+  document.getElementById("viewTSClosebtn").click();
+}
+
+var track_full=0; // 1 is on 0 is off
+var save_height=0;
+var save_width=0;
+function toggleExpandTSview(elt) {
+ 
+  track_full = !track_full;
+  if(track_full) {
+    let h = document.documentElement.clientHeight;
+    elt.innerHTML="Expand";
+    $('#modalTSDialog').removeClass('modal-full-dialog');
+    $('#modalTSContent').removeClass('modal-full-content');
+    save_height=document.getElementById("viewTSIfram").height;
+    save_width=document.getElementById("viewTSIfram").width;
+    let nh= Math.floor(save_height/2);
+    let nw= Math.floor(nh * 3/2);
+    document.getElementById("viewTSIfram").height=nh;
+    document.getElementById("viewTSIfram").width=nw;
+window.console.log("new width"+nw);
+window.console.log("new height"+nh);
+    } else {
+      elt.innerHTML="Shrink";
+      $('#modalTSDialog').addClass('modal-full-dialog');
+      $('#modalTSContent').addClass('modal-full-content');
+      var c=document.getElementById("modalTSContent");
+      var f=document.getElementById("modalTSFooter");
+      document.getElementById("viewTSIfram").height = save_height;
+      document.getElementById("viewTSIfram").width = save_width;
+window.console.log("old width"+nw);
+window.console.log("old height"+nh);
+  }
+}
+
+function resetExpandTSview() {
+  let elt=document.getElementById("viewTSExpandbtn");
+  if(track_full == 1) {
+    track_full=0;
+    elt.innerHTML="Shrink";
+    $('#modalTSDialog').addClass('modal-full-dialog');
+    $('#modalTSContent').addClass('modal-full-content');
+    var c=document.getElementById("modalTSContent");
+    var f=document.getElementById("modalTSFooter");
+    document.getElementById("viewTSIfram").height = save_height;
+    document.getElementById("viewTSIfram").width = save_width;
+  }
+}
+
+// XXX Downloadbtn at plotly inside
+function saveTSview() {
+  document.getElementById("viewTSIfram").contentDocument.getElementById("Downloadbtn").click();
+}
+// XXX
+function  toggleEReprTSview(elt) {
+}
+// XXX
+function  resetReprTSview() {
+}
+
