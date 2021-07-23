b5479e2a342bdb0a96d5d0f8a9bbaa9768d2c122
Mei-Hui Su <mei@MeiPro.local>
2021-07-23 08:18:16 -0700

    more ts plot tweaks

--- web/js/ts/myPlotly.js
+++ web/js/ts/myPlotly.js
@@ -4,15 +4,6 @@ jQuery(document).ready(function() {
   let frameHeight=window.innerHeight;
   let frameWidth=window.innerWidth;
 
-  let nh=frameHeight;
-  let nw= Math.floor(nh/3)*4;
-  if(nw > frameWidth) {
-    nw=frameWidth;
-    nh= Math.floor((nw/4)*3);
-  }
-
-window.console.log("width "+nw+" height "+nh);
-  // grab the params from the iframe data
   [URL, fname]=getCallingParams();
 
 window.console.log("in TS..("+URL+") ("+fname+")");
@@ -21,6 +12,6 @@ window.console.log("in TS..("+URL+") ("+fname+")");
 //  let myFname=[ 'ANA1.cgm.wmrss_igb14.pos' ];
 //  loadAndProcessFromFile(myURL,myFname,frameWidth,frameHeight);
 
-  loadAndProcessFromFile([URL],[fname],nw,nh);
+  loadAndProcessFromFile([URL],[fname]);
 
 }) // end of MAIN
--- web/js/ts/ts_util.js
+++ web/js/ts/ts_util.js
@@ -22,13 +22,13 @@ function ckExist(url) {
 };
 
 
-function loadAndProcessFromFile(ulist,nlist,figw,figh) {
+function loadAndProcessFromFile(ulist,nlist) {
   let sz=ulist.length;
 
 // assume there is just 1 for now
   let data = ckExist(ulist[0]);
   let plot_data=processPOS(0,nlist[0],data);
-  plotly_plot_pos(plot_data,figw,figh);
+  plotly_plot_pos(plot_data);
   
 /*** ???
   for(i=0;i<sz;i++) {
@@ -71,7 +71,33 @@ function getCallingParams() {
 
 
 // plotly 
-function plotly_plot_pos(pos_data,figw,figh) {
+function plotly_plot_pos(pos_data) {
+
+  let margin_left_offset=60;
+  let frameHeight=window.innerHeight;                                    
+  let frameWidth=window.innerWidth-margin_left_offset;
+
+  let frameWidth_min=800;
+
+  let nh=frameHeight;
+  let nw=Math.floor(nh/3)*3.5;
+  if(frameWidth < frameWidth_min) {
+     window.console.log("plotting screen width is too small, should be at least "+frameWidth_min);
+     nw=frameWidth_min;
+     nh= Math.floor((nw/3.5)*3);
+     } else {
+       if(nw > frameWidth) {
+          window.console.log("in recalculate..");
+          nw=frameWidth;
+          nh= Math.floor((nw/3.5)*3);
+       }
+  }
+
+  let margin_left_default=80;
+  let margin_top_default=100;
+
+  let margin_top=margin_top_default+Math.floor((frameHeight-nh)/2);
+  let margin_left=margin_left_default+margin_left_offset;
 
   let plot=pos_data[0].plot;   
 
@@ -79,7 +105,6 @@ function plotly_plot_pos(pos_data,figw,figh) {
   let pEast=plot[1];
   let pUp=plot[2];
 
-window.console.log("ts HERE..");
 
   let traceEast = { x: pEast.x,
                     y: pEast.y,
@@ -89,7 +114,11 @@ window.console.log("ts HERE..");
                     error_y: {
                           type: 'data',
                           array: pEast.yError,
-                          visible: true
+                          visible: true,
+    color: '#000000',
+    thickness: 1,
+    width: 1.5,
+    opacity: 0.8 
                     },
                     type: 'scatter' };
   let traceNorth= { x: pNorth.x,
@@ -101,9 +130,10 @@ window.console.log("ts HERE..");
                           type: 'data',
                           array: pNorth.yError,
                           visible: true,
-                          color: '#ff0000',
-                          thickness: 1, 
-                          width: 1
+    color: '#000000',
+    thickness: 1,
+    width: 1.5,
+    opacity: 0.8 
                     },
                     type: 'scatter' };
   let traceUp = { x: pUp.x,
@@ -115,14 +145,19 @@ window.console.log("ts HERE..");
                         type: 'data',
                         array: pUp.yError,
                         visible: true,
+    color: '#000000',
+    thickness: 1,
+    width: 1.5,
+    opacity: 0.8 
                   },
                   type: 'scatter' };
 
   var data = [traceEast, traceNorth, traceUp ];
   var layout = { 
-                 width: figw,
-                 height: figh,
-                 
+                 width: nw,
+                 height: nh,
+                 margin: { l:margin_left, t:margin_top },
+                 colorway: [ '#1f77b4','#1f77b4','#1f77b4'],
                  yaxis: {
 title: {text: 'East(mm)', font: { size:18,color:'#000000'}},
 showgrid: true,
@@ -130,7 +165,7 @@ zeroline: true,
 showline: true,
 mirror: 'allticks',
 tickes: 'inside',
-gridcolor: '#bdbdbd',
+gridcolor: '#f1f1f1',
 gridwidth: 1,
 zerolinecolor: '#969696',
 zerolinewidth: 1,
@@ -144,7 +179,7 @@ zeroline: true,
 showline: true,
 mirror: 'allticks',
 tickes: 'inside',
-gridcolor: '#bdbdbd',
+gridcolor: '#f1f1f1',
 gridwidth: 1,
 zerolinecolor: '#969696',
 zerolinewidth: 1,
@@ -158,7 +193,7 @@ zeroline: true,
 showline: true,
 mirror: 'allticks',
 tickes: 'inside',
-gridcolor: '#bdbdbd',
+gridcolor: '#f1f1f1',
 gridwidth: 1,
 zerolinecolor: '#969696',
 zerolinewidth: 1,
@@ -166,12 +201,17 @@ linecolor: '#636363',
 linewidth: 2
                          },
                  xaxis: {
+
+    tickmode: "linear",
+    tick0: '2000-01-01',
+    dtick: 'M24', // number of months
+
 showgrid: true,
 zeroline: true,
 showline: true,
 mirror: 'allticks',
 tickes: 'inside',
-gridcolor: '#bdbdbd',
+gridcolor: '#f1f1f1',
 gridwidth: 1,
 zerolinecolor: '#969696',
 zerolinewidth: 1,
@@ -179,13 +219,16 @@ linecolor: '#636363',
 linewidth: 2
                          },
                  xaxis2: {
+    tickmode: "linear",
+    tick0: '2000-01-01',
+    dtick: 'M24', // number of months
 matches: 'x',
 showgrid: true,
 zeroline: true,
 showline: true,
 mirror: 'allticks',
 tickes: 'inside',
-gridcolor: '#bdbdbd',
+gridcolor: '#f1f1f1',
 gridwidth: 1,
 zerolinecolor: '#969696',
 zerolinewidth: 1,
@@ -193,13 +236,16 @@ linecolor: '#636363',
 linewidth: 2
                          },
                  xaxis3: {
+    tickmode: "linear",
+    tick0: '2000-01-01',
+    dtick: 'M24', // number of months
 matches: 'x',
 showgrid: true,
 zeroline: true,
 showline: true,
 mirror: 'allticks',
 tickes: 'inside',
-gridcolor: '#bdbdbd',
+gridcolor: '#f1f1f1',
 gridwidth: 1,
 zerolinecolor: '#969696',
 zerolinewidth: 2,
@@ -208,12 +254,12 @@ linewidth: 2
                          },
                  grid: { rows: 3, columns: 1, pattern: 'independent' },
 annotations: [
-{ text: pEast.topLeft, showarrow:false, x:0, xref:"x domain",y:1.2, yref:"y domain"},
-{ text: pNorth.topLeft, showarrow:false, x:0, xref:"x domain",y:1.2, yref:"y2 domain"},
-{ text: pUp.topLeft, showarrow:false, x:0, xref:"x domain",y:1.2, yref:"y3 domain"},
-{ text: pEast.topRight, showarrow:false, x:1, xref:"x domain",y:1.2, yref:"y domain"},
-{ text: pNorth.topRight, showarrow:false, x:1, xref:"x domain",y:1.2, yref:"y2 domain"},
-{ text: pUp.topRight, showarrow:false, x:1, xref:"x domain",y:1.2, yref:"y3 domain"}
+{ text: pEast.topLeft, showarrow:false, x:0, xref:"x domain",y:1.15, yref:"y domain"},
+{ text: pNorth.topLeft, showarrow:false, x:0, xref:"x domain",y:1.15, yref:"y2 domain"},
+{ text: pUp.topLeft, showarrow:false, x:0, xref:"x domain",y:1.15, yref:"y3 domain"},
+{ text: pEast.topRight, showarrow:false, x:1, xref:"x domain",y:1.15, yref:"y domain"},
+{ text: pNorth.topRight, showarrow:false, x:1, xref:"x domain",y:1.15, yref:"y2 domain"},
+{ text: pUp.topRight, showarrow:false, x:1, xref:"x domain",y:1.15, yref:"y3 domain"}
 ]
                };
 
