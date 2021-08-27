0fe1b7b7d1c315e02852c2ef74a511a108621b09
Mei-Hui Su <mei@MeiPro.local>
2021-08-26 17:49:32 -0700

    tweak

--- web/php/search.php
+++ web/php/search.php
@@ -11,7 +11,7 @@ $criteria = json_decode($_REQUEST["q"]);
 //array_push($criteria, 35.0522);
 //array_push($criteria, -118.2437);
 
-if (is_object($criteria)) {
+if (is_object($criteria[0])) {
      $criteria = (array)$criteria[0];
 }
 //print_r($criteria);exit;
