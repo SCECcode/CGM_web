b2702ecdc68844f2f605e7bb7e5522eb98219ef4
Mei-Hui Su <mei@MeiPro.local>
2021-08-26 17:38:59 -0700

    fix search.php erorr

--- web/php/search.php
+++ web/php/search.php
@@ -11,14 +11,14 @@ $criteria = json_decode($_REQUEST["q"]);
 //array_push($criteria, 35.0522);
 //array_push($criteria, -118.2437);
 
-if (is_object($criteria[0])) {
-	$criteria = (array)$criteria[0];
+if (is_object($criteria)) {
+     $criteria = (array)$criteria[0];
 }
 //print_r($criteria);exit;
 
 try {
 //        print $cgm_insar->doTesting()->outputJSON();
-      print $cgm_insar->search($type, $criteria)->outputJSON();
+    print $cgm_insar->search($type, $criteria)->outputJSON();
 } catch (BadFunctionCallException $e) {
-	print "error";
+    print "error";
 }
