caf960bf595906d8b716caa9d9534d3989add31b
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2023-11-20 06:08:56 +0000

    update station_type

--- schema/CGM_data/setup_cgm.sql
+++ schema/CGM_data/setup_cgm.sql
@@ -29,7 +29,7 @@ alter table tmp1
 COPY tmp1(station_id, ref_date_time, ref_north_latitude, ref_east_longitude, ref_up, ref_velocity_north, ref_velocity_east, ref_velocity_up, first_epoch, last_epoch)
     FROM PROGRAM 'tail -n +8 /home/postgres/CGM/CGM_data/wus_gps_final_names.short.geocsv ' DELIMITER ',' CSV;
 
-UPDATE tmp1 set station_type = 'continuous';
+UPDATE tmp1 set station_type = 'NA';
 UPDATE tmp1 set cgm_version = '1';
 UPDATE tmp1 set source_filename = 'wus_gps_final_names.short.geocsv';
 
@@ -41,7 +41,7 @@ truncate table tmp1;
 COPY tmp1(station_id, ref_date_time, ref_north_latitude, ref_east_longitude, ref_up, ref_velocity_north, ref_velocity_east, ref_velocity_up, first_epoch, last_epoch)
     FROM PROGRAM 'tail -n +8 /home/postgres/CGM/CGM_data/crowell_campaign_velocities.short.geocsv' DELIMITER ',' CSV;
 
-UPDATE tmp1 set station_type = 'campaign';
+UPDATE tmp1 set station_type = 'NA';
 UPDATE tmp1 set cgm_version = '1';
 UPDATE tmp1 set source_filename = 'crowell_campaign_velocities.short.geocsv';
 
@@ -54,6 +54,13 @@ DELETE FROM cgm_gnss_station_velocities t2
 WHERE NOT EXISTS 
 (SELECT 1 FROM cgm_gnss_sites t1
  WHERE t1.name = t2.station_id);
+
+
+UPDATE CGM_gnss_station_velocities
+   SET station_type = 
+      ( select type FROM CGM_gnss_sites
+           where CGM_gnss_station_velocities.station_id = CGM_gnss_sites.name);	
+
 
 -----------------------
 COPY cgm_insar_tracks(file,track,color,bb1_lat,bb1_lon,bb2_lat,bb2_lon,bb3_lat,bb3_lon,bb4_lat,bb4_lon,geocode_increment,geocode_range,ref_lat,ref_lon)
