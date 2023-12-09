4d5d074e17bbce471adf6f65e6f683df7995f69c
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2023-12-09 01:59:30 +0000

    wip

--- schema/CGM_data/cgm_schema.sql
+++ schema/CGM_data/cgm_schema.sql
@@ -5,14 +5,14 @@ CREATE TABLE cgm_gnss_station_velocities (
     ulabel VARCHAR(10) NOT NULL,   -- 0001_s
     Dot VARCHAR(5) NOT NULL,       --* 0001
     stationType VARCHAR(15) NOT NULL,  -- survey or continuous
-    Ref_epoch  text,            --* 19941103120000
-    Ref_jday numeric,              -- 49659.5000
-    Ref_X numeric,                 -- -2581457.64570
-    Ref_Y numeric,                 -- -4600859.94210
-    Ref_Z numeric,                 -- 3572641.78449
-    Ref_Nlat numeric,              --* 34.2848530892
-    Ref_Elong numeric,             --* 240.7040208756
-    Ref_Up numeric,                --* 77.90925
+    Ref_epoch VARCHAR(14),       --* 19941103120000
+    Ref_jday float,              -- 49659.5000
+    Ref_X float,                 -- -2581457.64570
+    Ref_Y float,                 -- -4600859.94210
+    Ref_Z float,                 -- 3572641.78449
+    Ref_Nlat float,              --* 34.2848530892
+    Ref_Elong float,             --* 240.7040208756
+    Ref_Up float,                --* 77.90925
     dXOdt text,                    -- NaN
     dYOdt text,                    -- NaN
     dZOdt text,                    -- NaN
@@ -22,18 +22,18 @@ CREATE TABLE cgm_gnss_station_velocities (
     Rxy text,                      -- NaN
     Rxz text,                      -- NaN
     Rzy text,                      -- NaN
-    dNOdt numeric,                 --* 0.03578
-    dEOdt numeric,                 --* -0.03713
-    dUOdt numeric,                 --* -0.00155
+    dNOdt float,                 --* 0.03578
+    dEOdt float,                 --* -0.03713
+    dUOdt float,                 --* -0.00155
     SNd text,                      -- 0.00152
     SEd text,                      -- 0.00147
     SUd text,                      -- 0.00573
     Rne text,                      -- -0.000
     Rnu text,                      -- NaN
     Reu text,                      -- NaN
-    first_epoch text,           --* 19920605120000
-    last_epoch text,            --* 19941103120000
-    n_observations  numeric,       -- 6
+    first_epoch VARCHAR(14),     --* 19920605120000
+    last_epoch VARCHAR(14),      --* 19941103120000
+    n_observations  integer,           -- 6
     cgm_version text
 );
 
--- web/js/cgm_gnss.js
+++ web/js/cgm_gnss.js
@@ -111,7 +111,7 @@ var CGM_GNSS = new function () {
     };
 
     var tablePlaceholderRow = `<tr id="placeholder-row">
-                        <td colspan="12">Metadata for selected points will appear here.</td>
+                        <td colspan="11">Metadata for selected points will appear here.</td>
                     </tr>`;
 
     this.activateData = function() {
@@ -681,7 +681,6 @@ window.console.log(" Clicked on a layer--->"+ event.layer.scec_properties.statio
         html += `<td class="cgm-data-click">${coordinates.lat}</td>`;
         html += `<td class="cgm-data-click">${coordinates.lng}</td>`;
         html += `<td class="cgm-data-click">${layer.scec_properties.type} </td>`;
-        html += `<td class="cgm-data-click">${layer.scec_properties.group} </td>`;
         html += `<td class="cgm-data-click">${layer.scec_properties.vel_east} </td>`;
         html += `<td class="cgm-data-click">${layer.scec_properties.vel_north} </td>`;
         html += `<td class="cgm-data-click">${layer.scec_properties.horizontalVelocity}</td>`;
@@ -1064,12 +1063,11 @@ window.console.log("generateResultsTable..");
                         <th class="hoverColor" onClick="sortMetadataTableByRow(2,'n')">Lat&nbsp<span id='sortCol_2' class="fas fa-angle-down"></span></th>
                         <th class="hoverColor" onClick="sortMetadataTableByRow(3,'n')">Lon&nbsp<span id='sortCol_3' class="fas fa-angle-down"></span></th>
                         <th style="width:6rem">Type</th>
-                        <th style="width:3rem">Group</th>
-                        <th class="hoverColor" onClick="sortMetadataTableByRow(6,'n')">East Vel&nbsp<span id='sortCol_5' class="fas fa-angle-down"></span></th>
-                        <th class="hoverColor" onClick="sortMetadataTableByRow(7,'n')">North Vel&nbsp<span id='sortCol_6' class="fas fa-angle-down"></span></th>
-                        <th class="hoverColor" onClick="sortMetadataTableByRow(8,'n')">Horizontal&nbsp<span id='sortCol_7' class="fas fa-angle-down"></span><br>Vel (mm/yr)</th>
-                        <th class="hoverColor" onClick="sortMetadataTableByRow(9,'n')">Azimuth&nbsp<span id='sortCol_8' class="fas fa-angle-down"></span></th>
-                        <th class="hoverColor" onClick="sortMetadataTableByRow(10,'n')">Vertical Vel&nbsp<span id='sortCol_9' class="fas fa-angle-down"></span><br>(mm/yr)</th>
+                        <th class="hoverColor" onClick="sortMetadataTableByRow(5,'n')">East Vel&nbsp<span id='sortCol_5' class="fas fa-angle-down"></span></th>
+                        <th class="hoverColor" onClick="sortMetadataTableByRow(6,'n')">North Vel&nbsp<span id='sortCol_6' class="fas fa-angle-down"></span></th>
+                        <th class="hoverColor" onClick="sortMetadataTableByRow(7,'n')">Horizontal&nbsp<span id='sortCol_7' class="fas fa-angle-down"></span><br>Vel (mm/yr)</th>
+                        <th class="hoverColor" onClick="sortMetadataTableByRow(8,'n')">Azimuth&nbsp<span id='sortCol_8' class="fas fa-angle-down"></span></th>
+                        <th class="hoverColor" onClick="sortMetadataTableByRow(9,'n')">Vertical Vel&nbsp<span id='sortCol_9' class="fas fa-angle-down"></span><br>(mm/yr)</th>
 
 <!-- 
 <button id="infoBtn" class="btn cgm-small-btn" data-toggle="modal" data-target="#modalazimuth"><span class="fas fa-info-circle"></span> 
